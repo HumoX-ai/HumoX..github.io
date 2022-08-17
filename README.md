1713ee70c926159d17fffccbe44aca7364c519de
Humoyun2005 <abduodirovhumoyun@gmail.com>
2022-08-17 14:24:16 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+this is the submodule 
+"# react-dom" 
---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pt-4">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 mb-10" >
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Men 2005-yil 19-dekabrda Toshkent viloyati Yangiyo`l tumanida tug`ilganman. dasturchilikka 2020-yil kirib kelganman. Hozirgacha o`iqb taqsil olyapman. Mening kelajakdagi orzuyim Senior dasturchi bo`lish😎.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          pariatur, vel similique sint, nobis aspernatur ut praesentium
+          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
+          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
+          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
+          magni quo! Eum cupiditate debitis labore.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
